48b083d849b99773ded5cef1472301645c268d85
Your Name <renaud.dubuis@decryptage.net>
2025-10-01 14:27:28 +0200

    Config Project (css -> scss )

--- projects/lib-axy/src/public-api.ts
+++ projects/lib-axy/src/public-api.ts
@@ -1,6 +1,15 @@
 /*
  * Public API Surface of lib-axy
+ * Tout ce qui doit etre accessible par les applications qui consomment la librarie
+ * (components, services, modules, ...)
+ * Doit etre exporte ici
  */
 
-export * from './lib/lib-axy.service';
-export * from './lib/lib-axy.component';
+
+/**
+ * Générer dans un dossier "components" un composant header et le rendre accessible via cette API publique
+ * ng generate component components/header --project=lib-axy
+ * ng build --project lib-axy
+ */
+
+export * from './lib/components/header/header.component';
