700265e639eb7c01b0c483981185964df446b364
Your Name <renaud.dubuis@decryptage.net>
2025-10-03 16:22:56 +0200

    Color Fix

--- projects/lib-axy/src/lib/directives/axy-base-css.directive.ts
+++ projects/lib-axy/src/lib/directives/axy-base-css.directive.ts
@@ -14,7 +14,7 @@ export class AxyBaseCssDirective implements OnInit {
     s.id = 'axy-base-css';
     s.textContent = `
       :root{ --ui-surface:#0f1524; --ui-surface-2:#151b2b; --ui-text:#eef3ff; --ui-muted:#a8b2c8; --ui-radius:14px; --ui-shadow:0 10px 28px rgba(8,12,20,.28); --ui-pad:14px; }
-      html,body{ margin:0; font-family: ui-sans-serif, system-ui, -apple-system, "Segoe UI", Roboto, Inter, "Noto Sans", "Helvetica Neue", Arial, "Apple Color Emoji", "Segoe UI Emoji"; background: var(--ui-surface); color: var(--ui-text); }
+      html,body{ margin:0; font-family: ui-sans-serif, system-ui, -apple-system, "Segoe UI", Roboto, Inter, "Noto Sans", "Helvetica Neue", Arial, "Apple Color Emoji", "Segoe UI Emoji"; background:  #79d0ffff; color: var(--ui-text); }
     `;
     this.doc.head.appendChild(s);
   }
