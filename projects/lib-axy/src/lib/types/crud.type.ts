a5826efd691445069f510bae855eeaa87fc7fdfb
Your Name <renaud.dubuis@decryptage.net>
2025-10-03 11:57:42 +0200

    Fix

--- projects/lib-axy/src/lib/types/crud.type.ts
+++ projects/lib-axy/src/lib/types/crud.type.ts
@@ -11,7 +11,7 @@ interface CrudService<T extends WithUniqueId>/* contrainte de type */ {
 
   create: (data: Omit<T, 'id'>) => Promise<HumanResponse<T>>;
 
-  read: (id?: T['id']) => Promise<HumanResponse<T>>;
+  read: (id?: T['id']) => Promise<HumanResponse<T | T[]>>;
 
   update: (target: T, data: Partial<Omit<T, 'id'>>) => Promise<HumanResponse<T>>;
 
