ae6ada1dc4ee057408064f89edae7c658c8a90c3
Your Name <renaud.dubuis@decryptage.net>
2025-10-03 14:53:51 +0200

    Update Alias

--- src/app/containers/product-list/product-list.container.ts
+++ src/app/containers/product-list/product-list.container.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ProductService } from '@services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './product-list.container.scss'
 })
 export class ProductListContainer {
-
+  productService = inject(ProductService);
 }
